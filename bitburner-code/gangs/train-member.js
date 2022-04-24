038dd7601b08376508698408be2f84f41189ea96
johnsozm <z.m.johnson@protonmail.com>
2022-04-24 11:37:25 -0600

    Added script to automatically train gang members

---
+++ bitburner-code/gangs/train-member.js
@@ -0,0 +1,65 @@
+import { GANGS } from "/CONFIG.js"
+
+/**
+ * Trains a gang member to the specified minimum stats.
+ * 
+ * @param {ns} ns Netscript object
+ */
+export async function main(ns) {
+    if (ns.args.length != 2) {
+        ns.tprint("Usage: train-member.js [member name] [skill level]");
+        return;
+    }
+
+    if (!GANGS) {
+        ns.tprint("Gangs must be unlocked to use this call.");
+        return;
+    }
+
+    const member = ns.args[0];
+    const targetLevel = parseInt(ns.args[1]);
+
+    if (!ns.gang.getMemberNames().includes(member)) {
+        ns.tprint("No such gang member " + member);
+        return;
+    }
+
+    while (true) {
+        const stats = ns.gang.getMemberInformation(member);
+        const ascensionResults = ns.gang.getAscensionResult(member);
+
+        const hackDone = stats.hack >= targetLevel;
+        const combatDone = (stats.agi >= targetLevel) && (stats.def >= targetLevel)
+                        && (stats.dex >= targetLevel) && (stats.str >= targetLevel);
+        const chaDone = stats.cha >= targetLevel;
+
+        const hackAscend = ascensionResults == null ? false : ascensionResults.hack >= 1.5 * stats.hack_asc_mult;
+        const combatAscend = ascensionResults == null ? false : (ascensionResults.agi >= 1.5 * stats.agi_asc_mult)
+                            && (ascensionResults.def >= 1.5 * stats.def_asc_mult)
+                            && (ascensionResults.dex >= 1.5 * stats.dex_asc_mult)
+                            && (ascensionResults.str >= 1.5 * stats.str_asc_mult);
+        const chaAscend = ascensionResults == null ? false : ascensionResults.cha >= 1.5 * stats.cha_asc_mult;
+
+        if (hackDone && combatDone && chaDone) {
+            ns.gang.setMemberTask(member, "Unassigned");
+            break;
+        }
+        else if (!hackDone  && !hackAscend) {
+            ns.gang.setMemberTask(member, "Train Hacking");
+        }
+        else if (!combatDone && !combatAscend) {
+            ns.gang.setMemberTask(member, "Train Combat");
+        }
+        else if (!chaDone && !chaAscend) {
+            ns.gang.setMemberTask(member, "Train Charisma");
+        }
+        else {
+            ns.gang.ascendMember(member);
+        }
+
+        await ns.sleep(10000);
+    }
+
+    ns.tprint("Finished training " + member);
+}
+
