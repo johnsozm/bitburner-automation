e62164af80590478a870870179f8b88092e0e1dd
johnsozm <z.m.johnson@protonmail.com>
2022-04-24 17:56:22 -0600

    Added delayed hack, weaken, and grow scripts

---
+++ bitburner-code/hacks/weaken-delayed.js
@@ -0,0 +1,18 @@
+/**
+ * Script which executes a weaken after a specified delay.
+ * Should only be called by batcher.
+ * 
+ * @param {ns} ns Netscript object
+ */
+ export async function main(ns) {
+    if (ns.args.length != 2) {
+        ns.tprint("Usage: weaken-delayed.js [target] [delay (ms)]");
+        return;
+    }
+
+    const target = ns.args[0];
+    const delay = parseInt(ns.args[1]);
+
+    await ns.sleep(delay);
+    await ns.weaken(target);
+}
