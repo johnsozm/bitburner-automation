deb121fcc03b41ce56c66319e88831d746539cd1
johnsozm <z.m.johnson@protonmail.com>
2022-04-24 19:30:47 -0600

    Implemented thread and delay calculations

--- bitburner-code/hacks/hack-mgr-batch.js
+++ bitburner-code/hacks/hack-mgr-batch.js
@@ -1,5 +1,7 @@
 import { getAllServers, rootServer } from "/utils/server-functions.js";
 
+/**Delay to aim for between hack/weaken/grow steps.*/
+const INTER_STEP_DELAY = 500;
 /**Delay to wait between running successive batches.*/
 const INTER_BATCH_DELAY = 1000;
 /**Delay to wait between main loops.*/
@@ -47,14 +49,14 @@ export async function main(ns) {
                 continue;
             }
             servers[i].threads = calculateBatchThreads(ns, servers[i].name);
-            servers[i].delays = calculateBatchDelays(ns, servers[i].name);
+            servers[i].delays = calculateBatchDelays(ns, servers[i].name, servers[i].minSecurity, servers[i].maxMoney);
             servers[i].runningPids = updateRunningPIDs(ns, servers[i].runningPids);
             servers[i].batchCost = (servers[i].threads.hack * costs.hack) + (servers[i].threads.grow * costs.grow)
                                     + ((servers[i].threads.weaken_1 + servers[i].threads.weaken_2) * costs.weaken);
             totalBatchCost += servers[i].batchCost;
         }
 
-        //Get available and total space on all purchased servers + home
+        //Get available and total space on all purchased servers
         var totalSpace = 0;
         var freeSpace = ns.getPurchasedServers.map(function(hostname) {
             totalSpace += getServerMaxRam(hostname);
@@ -62,12 +64,6 @@ export async function main(ns) {
                 name: hostname,
                 free: ns.getServerMaxRam(hostname) - getServerUsedRam(hostname)
             }
-        });
-
-        totalSpace += getServerMaxRam("home") - 100;
-        freeSpace.push({
-            name: "home",
-            free: getServerMaxRam("home") - getServerUsedRam("home") - 100
         });
 
         //Update batch count & check total usage against total memory available
@@ -134,11 +130,28 @@ function updateRunningPIDs(ns, pids) {
  * 
  * @param {ns} ns Netscript object
  * @param {string} hostname Server hostname
+ * @param {number} minSecurity The minimum security level on the server
+ * @param {number} maxMoney The maximum money on the server
  * @returns The thread parameters that should be used for the next batch on this host
  */
-function calculateBatchThreads(ns, hostname) {
-    //TODO: Implement
-    return {};
+function calculateBatchThreads(ns, hostname, minSecurity, maxMoney) {
+    //TODO: Deal with possibility of cores != 1 when running from home
+    var hackThreads = Math.floor(ns.hackAnalyzeThreads(hostname, 0.5 * maxMoney));
+    const growThreads = Math.ceil(ns.growthAnalyze(hostname, 2.0, 1));
+    const hackSecurity = ns.hackAnalyzeSecurity(hackThreads, hostname);
+    const growSecurity = ns.growthAnalyzeSecurity(growThreads, hostname);
+    const weaken1Threads = Math.ceil(hackSecurity / 0.05) + 1;
+    const weaken2Threads = Math.ceil(growSecurity / 0.05) + 1;
+
+    const shouldHack = (ns.getServerMoneyAvailable(hostname) >= 0.75 * maxMoney)
+                        && (ns.getServerSecurityLevel(hostname) <= 1.5 * minSecurity); 
+
+    return {
+        hack: shouldHack ? hackThreads : 0,
+        weaken_1: weaken1Threads,
+        grow: growThreads,
+        weaken_2: weaken2Threads
+    };
 }
 
 /**
@@ -149,8 +162,16 @@ function calculateBatchThreads(ns, hostname) {
  * @returns The delay parameters that should be used for the next batch on this host
  */
 function calculateBatchDelays(ns, hostname) {
-    //TODO: Implement
-    return {};
+    const hackTime = ns.getHackTime(hostname);
+    const growTime = ns.getGrowTime(hostname);
+    const weakenTime = ns.getWeakenTime(hostname);
+
+    return {
+        hack: weakenTime - hackTime - INTER_STEP_DELAY,
+        weaken_1: 0,
+        grow: weakenTime - growTime + INTER_STEP_DELAY,
+        weaken_2: 2 * INTER_STEP_DELAY
+    };
 }
 
 /**
