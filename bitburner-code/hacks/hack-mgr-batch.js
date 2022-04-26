f1e3d59d7f0ff15cae95cef642750a45f9cc355d
johnsozm <z.m.johnson@protonmail.com>
2022-04-26 11:50:09 -0600

    Massive fixed to get batch manager working

--- bitburner-code/hacks/hack-mgr-batch.js
+++ bitburner-code/hacks/hack-mgr-batch.js
@@ -6,6 +6,8 @@ const INTER_STEP_DELAY = 500;
 const INTER_BATCH_DELAY = 1000;
 /**Delay to wait between main loops.*/
 const LOOP_DELAY = 10000;
+/**Delay to wait between servers to avoid  locking up BitBurner*/
+const SERVER_DELAY = 100;
 
 /**
  * Hack manager which generates timed batches of hack/grow/weaken calls.
@@ -52,7 +54,6 @@ export async function main(ns) {
         }
 
         //Get updated batch parameters & running PID's for all rooted servers
-        var totalBatchCost = 0;
         for (let i = 0; i < servers.length; i++) {
             if (!servers[i].rooted) {
                 continue;
@@ -61,8 +62,7 @@ export async function main(ns) {
             servers[i].delays = calculateBatchDelays(ns, servers[i].name);
             servers[i].runningPids = updateRunningPIDs(ns, servers[i].runningPids);
             servers[i].batchCost = (servers[i].threads.hack * costs.hack) + (servers[i].threads.grow * costs.grow)
-                                    + ((servers[i].threads.weaken_1 + servers[i].threads.weaken_2) * costs.weaken);
-            totalBatchCost += servers[i].batchCost;
+                                    + ((servers[i].threads.weaken1 + servers[i].threads.weaken2) * costs.weaken);
         }
 
         //Get available and total space on all purchased servers
@@ -92,24 +92,30 @@ export async function main(ns) {
 
         //Execute all batches
         for (let i = 0; i < servers.length; i++) {
-            if (!servers[i].rooted || servers[i].runningPids.length > 0) {
+            if (!servers[i].rooted || servers[i].runningPids.length > 0 || servers[i].maxMoney == 0) {
                 continue;
             }
 
             var allPids = [];
             for (let j = 0; j < numBatches; j++) {
-                thisPids = executeBatch(ns, servers[i].name, servers[i].threads, servers[i].delays, j * INTER_BATCH_DELAY, freeSpace, costs);
-
-                if (thisPids.length == 0) {
-                    ns.tprint("ERROR: No batches initiated. Exiting.");
-                    return;
-                }
+                var thisPids = executeBatch(ns, servers[i].name, servers[i].threads, servers[i].delays, j * INTER_BATCH_DELAY, freeSpace, costs);
+                var anyStarted = false;
 
                 thisPids.forEach((pid) => {
                     allPids.push(pid);
+                    if (pid != 0) {
+                        anyStarted = true;
+                    }
                 });
+                
+                if (thisPids.length == 0 || !anyStarted) {
+                    ns.tprint("ERROR: No batches initiated for " + servers[i].name + ". Exiting.");
+                    return;
+                }
             }
-            servers[i].runningPids = thisPids;
+            servers[i].runningPids = allPids;
+
+            await ns.sleep(SERVER_DELAY);
         }
 
         await ns.sleep(LOOP_DELAY);
@@ -145,7 +151,7 @@ function updateRunningPIDs(ns, pids) {
  */
 function calculateBatchThreads(ns, hostname, minSecurity, maxMoney) {
     //TODO: Deal with possibility of cores != 1 when running from home
-    var hackThreads = Math.floor(ns.hackAnalyzeThreads(hostname, 0.5 * maxMoney));
+    const hackThreads = Math.ceil(0.5 / ns.formulas.hacking.hackPercent(ns.getServer(hostname), ns.getPlayer()));
     const growThreads = Math.ceil(ns.growthAnalyze(hostname, 2.0, 1));
     const hackSecurity = ns.hackAnalyzeSecurity(hackThreads, hostname);
     const growSecurity = ns.growthAnalyzeSecurity(growThreads, hostname, 1);
@@ -157,9 +163,9 @@ function calculateBatchThreads(ns, hostname, minSecurity, maxMoney) {
 
     return {
         hack: shouldHack ? hackThreads : 0,
-        weaken_1: weaken1Threads,
+        weaken1: weaken1Threads,
         grow: growThreads,
-        weaken_2: weaken2Threads
+        weaken2: weaken2Threads
     };
 }
 
@@ -177,9 +183,9 @@ function calculateBatchDelays(ns, hostname) {
 
     return {
         hack: weakenTime - hackTime - INTER_STEP_DELAY,
-        weaken_1: 0,
+        weaken1: 0,
         grow: weakenTime - growTime + INTER_STEP_DELAY,
-        weaken_2: 2 * INTER_STEP_DELAY
+        weaken2: 2 * INTER_STEP_DELAY
     };
 }
 
@@ -200,8 +206,13 @@ function executeBatch(ns, target, threads, delays, batchDelay, freeSpace, costs)
     var remaining = Object.assign({}, threads);
     var pids = [];
 
+    if (threads.hack + threads.weaken1 + threads.grow + threads.weaken2 == 0) {
+        ns.tprint("ERROR: Called executeBatch with 0 threads");
+        return [];
+    }
+
     for (let i = 0; i < freeSpace.length; i++) {
-        if (freeSpace[i].free < minCost || !ns.serverExists(target)) {
+        if (freeSpace[i].free < minCost || !ns.serverExists(freeSpace[i].name)) {
             continue;
         }
 
@@ -213,7 +224,8 @@ function executeBatch(ns, target, threads, delays, batchDelay, freeSpace, costs)
             }
             else {
                 pids.push(pid);
-                freeSpace[i] -= hackThreads * costs.hack;
+                freeSpace[i].free -= hackThreads * costs.hack;
+                remaining.hack -= hackThreads;
             }
         }
         else if (remaining.weaken1 > 0) {
@@ -224,7 +236,8 @@ function executeBatch(ns, target, threads, delays, batchDelay, freeSpace, costs)
             }
             else {
                 pids.push(pid);
-                freeSpace[i] -= weakenThreads * costs.weaken;
+                freeSpace[i].free -= weakenThreads * costs.weaken;
+                remaining.weaken1 -= weakenThreads;
             }
         }
         else if (remaining.grow > 0) {
@@ -235,7 +248,8 @@ function executeBatch(ns, target, threads, delays, batchDelay, freeSpace, costs)
             }
             else {
                 pids.push(pid);
-                freeSpace[i] -= growThreads * costs.grow;
+                freeSpace[i].free -= growThreads * costs.grow;
+                remaining.grow -= growThreads;
             }
         }
         else if (remaining.weaken2 > 0) {
@@ -246,7 +260,8 @@ function executeBatch(ns, target, threads, delays, batchDelay, freeSpace, costs)
             }
             else {
                 pids.push(pid);
-                freeSpace[i] -= weakenThreads * costs.hack;
+                freeSpace[i].free -= weakenThreads * costs.weaken;
+                remaining.weaken2 -= weakenThreads;
             }
         }
         else {
