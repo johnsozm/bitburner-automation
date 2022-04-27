5e6d549ff7bcf3ba220f91f5b70c2128dc9a6b00
johnsozm <z.m.johnson@protonmail.com>
2022-04-27 13:35:43 -0600

    Added script for auto-managing wanted level

---
+++ bitburner-code/gangs/maintain-wanted.js
@@ -0,0 +1,37 @@
+import { GANGS } from "/CONFIG.js"
+
+/**
+ * Uses a single gang member to maintain wanted losses between 0 and 1/12.
+ * 
+ * @param {ns} ns Netscript object
+ */
+export async function main(ns) {
+    if (ns.args.length != 3) {
+        ns.tprint("Usage: maintain-wanted.js [member name] [management task] [default task]");
+        return;
+    }
+    if (!GANGS) {
+        ns.tprint("Gangs must be unlocked to use this call.");
+        return;
+    }
+
+    var shrinkingWanted = false;
+
+    while (true) {
+        if (shrinkingWanted) {
+            ns.gang.setMemberTask(ns.args[0], ns.args[1]);
+            if (ns.gang.getGangInformation().wantedPenalty == 0) {
+                shrinkingWanted = false;
+            }
+        }
+        else {
+            ns.gang.setMemberTask(ns.args[0], ns.args[2]);
+            if (ns.gang.getGangInformation().wantedPenalty >= 1.0/12.0) {
+                shrinkingWanted = true;
+            }
+        }
+
+        await ns.sleep(10000);
+    }
+    
+}
