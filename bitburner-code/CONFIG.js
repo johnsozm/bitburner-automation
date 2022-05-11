b18f5f4cc85de6861e25162c9fb55df6136dc258
johnsozm <z.m.johnson@protonmail.com>
2022-05-11 14:07:06 -0600

    Updated singularity flag

--- bitburner-code/CONFIG.js
+++ bitburner-code/CONFIG.js
@@ -2,6 +2,6 @@
  * File containing configuration variables which scripts can use to enable/disable features.
  */
 
-export const SINGULARITY = false;
+export const SINGULARITY = true;
 export const GANGS = true;
 export const CORPORATIONS = true;
